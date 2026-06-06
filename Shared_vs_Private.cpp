f83765aea39e07d8379bc35a9c663981401356e6
Samer Saad <112403636+samersad@users.noreply.github.com>
2026-06-06 19:51:47 +0300

    Add files via upload

---
+++ Shared_vs_Private.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <omp.h>
+using namespace std;
+
+int main() {
+    int imax = 10;
+    int ifirst = 10;
+    int i2;
+    int j[10];
+
+    // بدون private - race condition
+#pragma omp parallel for
+    for (int i = 0; i < imax; i++) {
+        i2 = 2 * i;
+        j[i] = ifirst + i2;
+    }
+
+    cout << "Without private (may be wrong):" << endl;
+    for (int i = 0; i < imax; i++)
+        cout << "j[" << i << "] = " << j[i] << endl;
+
+    // مع private - صح
+#pragma omp parallel for private(i2)
+    for (int i = 0; i < imax; i++) {
+        i2 = 2 * i;
+        j[i] = ifirst + i2;
+    }
+
+    cout << "\nWith private(i2) (correct):" << endl;
+    for (int i = 0; i < imax; i++)
+        cout << "j[" << i << "] = " << j[i] << endl;
+
+    return 0;
+}
